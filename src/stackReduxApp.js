cf054d535af0237302677668b3ec3ec86c22cf37
ddeak <daniel_deak@live.com>
2017-01-29 14:14:48 +0000

    Fixed error.

--- src/stackReduxApp.js
+++ src/stackReduxApp.js
@@ -1,5 +1,4 @@
 import { applyMiddleware, combine, combineReducers } from 'redux'
-import thunk from 'redux-thunk'
 import websocket from './middleware'
 import isPlainObject from 'lodash/isPlainObject'
 import isFunction from 'lodash/isFunction'
